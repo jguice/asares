fbdaf8dec1f0f8ab512613ffdc7cd829aed04d12
Josh Guice <josh@wizory.com>
2019-02-04 07:17:13 -0800

    fix(webserver): update yum cache

--- webserver/recipes/default.rb
+++ webserver/recipes/default.rb
@@ -1,6 +1,12 @@
 app = search(:aws_opsworks_app).first
 app_path = "/srv/#{app['shortname']}"
 
+# ensure yum package cache is up-to-date
+execute "yum check-update" do
+  command "yum check-update"
+  action :nothing
+end
+
 package 'nginx' do
   action :install
 end
